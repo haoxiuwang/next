c9fb294b183b48f3289e79716c1b1430173c7f6e
feiwang <fei.aomoxo@gmail.com>
2025-07-24 00:04:19 +0800

    update

--- framework/Link.jsx
+++ framework/Link.jsx
@@ -1,7 +1,8 @@
 import { navigate } from "./useRoute";
 export default function Link({href,onClick,className,children,...props}) {
     return <button {...{props}} onClick={(e)=>{
-        if(onClick)onClick()
-            navigate(href)
+        if(onClick)
+            if(onClick())return        
+        navigate(href)
     }} className={`cursor-pointer ${className}`}>{children}</button>
 }
